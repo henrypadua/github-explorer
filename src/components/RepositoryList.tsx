a416524593950170f4632acc16559d4eaddc2a1a
henrypadua <henrypadua>
2021-05-27 23:08:25 -0300

    Instalando o TypeScript e migrando o código

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,16 @@ import { useState, useEffect } from "react";
 
 import "../styles/repositories.scss";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  // <Repository> - Indica o tipo do meu estado
+  // <Repository[]> - Indica que o estado ira receber uma lista de repositórios.
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch("https://api.github.com/orgs/rocketseat/repos")
